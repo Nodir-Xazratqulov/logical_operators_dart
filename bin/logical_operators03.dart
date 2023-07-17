65851066d66762ed7e4d0aad1217df6bdf3ac4a2
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-17 11:51:03 +0500

    logic-3

--- bin/logical_operators03.dart
+++ bin/logical_operators03.dart
@@ -8,5 +8,13 @@ Given two integers a, b,  check the following statement "Each of the numbers 'a'
     Returns:
         bool: answer
 */
-
-void main() {}
+bool func(int a, int b){
+  if(a<0 && b<0){
+    return true;
+  } else{
+    return false;
+  }
+}
+void main() {
+  print(func(-2, -3));
+}
