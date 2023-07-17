a5411813b730a47acd7f6523bcde5763cbf12e4e
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-17 11:56:53 +0500

    logic-18

--- bin/logical_operators18.dart
+++ bin/logical_operators18.dart
@@ -8,4 +8,23 @@ Given a five-digit integer a, check the following statement "All digits of the n
         bool: answer
 */
 
-void main() {}
+func(int n){
+  int a = n%10; //5
+  int b = n~/10;
+  int c = b%10; //4
+  int d = b~/10; 
+  int e = d%10; //3
+  int f = d~/10;
+  int g = f%10; //2
+  int i = f~/10; //1
+
+  // a=5, c=4, e=3, g=2, i=1
+
+  if(a<c && c<e && e<g && g<i){
+    return true;
+  }
+  return false;
+}
+void main() {
+  print(func(97531));
+}
